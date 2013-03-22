dbeee5334115f72ccbfdf87b58aa855ad2612dbe
wcc723 <agito723@gmail.com>
2013-03-22 18:03:18 +0800

    zoom製作中

---
+++ js/aZoomIn.js
@@ -0,0 +1,57 @@
+﻿;(function($){
+	//document.ondragstart = function () { return false; }; 
+	$.fn.aZoomIn=function(settings){
+		var _defaultSettings={
+		zlimg : 'zlimg' //zoom In css
+
+		};		
+		var _settings=$.extend(_defaultSettings,_settings);
+		return this.each(function(){
+			aZoomIn($(this),_settings);
+		});
+		function aZoomIn($this,_a){
+			var $block = $this;
+			var $img = $block.find('img');		
+			var ZoomIn_link= $img.attr('src');		
+			var zlimg = '.'+_a.zlimg;
+			
+			//路徑
+			var srcPathArray = ZoomIn_link.split('/');
+			var imgFile = srcPathArray[srcPathArray.length-1];
+			var imgArray = imgFile.split('.');
+			var imgName =imgArray[0];
+			var imgExt = imgArray[1];
+			var srcPath = ZoomIn_link.split(imgFile , 1);
+			
+			
+			
+			$img.click(function(){		
+				var largeImg=srcPath+imgName+'Large.'+imgExt
+				$block.append("<div class='"+zlimg+"'><a href='#' class='exitZoomIn'>exit</a><img src='"+largeImg+"'> </div>");	
+				//var $imgbox = $block.find('.'+_a.zlimg);
+			});
+			 $('.exitZoomIn').click(function(){
+				 alert (132131)
+				 	return false;
+				 })
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		}
+	};
+})(jQuery);
---
+++ js/atogglePanel.js
@@ -0,0 +1,41 @@
+﻿;(function($){
+	document.ondragstart = function () { return false; }; 
+	$.fn.a_togglePanel=function(settings){
+		var _defaultSettings={
+		animateMode: 'slider', 
+		downSpeed: 200,
+		title:'.title',
+		content:'.article',
+		single: true 
+		};		
+		var _settings=$.extend(_defaultSettings,settings);
+		return this.each(function(){
+			a_togglePanel($(this),_settings);
+		});
+		function a_togglePanel($this,_a){
+			var $block = $this,					
+			_isComplete = true;			
+			_isComplete[$this] = true; 
+			
+			$this.find(_a.title).click(function(){
+			if (_isComplete[$this] == false)	 return false;		
+			_isComplete[$this] = false; 
+			if($(this).parent().find(_a.content).css('display') =='none'){
+				if (_a.single == true) {
+						$block.find('.selected').removeClass('selected').slideUp(_a.downSpeed);
+					}				
+				$(this).parent().find(_a.content).slideDown(_a.downSpeed,function(){
+					$(this).parent().find(_a.content).addClass('selected')
+					_isComplete[$this] = true; 
+					});
+				}else {
+					$(this).parent().find(_a.content).removeClass('selected').slideUp(_a.downSpeed,function(){
+						_isComplete[$this] = true; 
+						});
+					};
+					
+				});	
+							
+		}
+	};
+})(jQuery);
